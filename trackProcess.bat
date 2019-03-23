40a7ab67c3a2baddbae91bcb5e7f5404ad32412d
Shashank <kaavla@gmail.com>
2019-03-22 19:07:24 -0700

    added git commands

--- trackProcess.bat
+++ trackProcess.bat
@@ -2,3 +2,8 @@
 tasklist /FO CSV > input.txt
 (for /f "tokens=1,* delims=," %%a in (input.txt) do echo %%a) > output.txt
 sort /unique output.txt > procList.txt
+del input.txt
+del output.txt
+git add procList.txt
+git commit -m "ProcList Update"
+git push origin master
